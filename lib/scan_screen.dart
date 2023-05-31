271e06a7f704c7dc7355284722ae1d998c2bd8ea
Viktor Shen <viktor@Viktors-MacBook-Pro.fritz.box>
2023-05-31 19:14:46 +0200

    scanner part 2

--- lib/scan_screen.dart
+++ lib/scan_screen.dart
@@ -45,6 +45,128 @@ class _ScanScreenState extends State<ScanScreen> {
               ),
             ),
           ),
+          Center(
+            child: SizedBox(
+              width: 320,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            // 'GestureDetector' widget is used to detect tap events.
+                            onTap: () {
+                              // This function will be called when the user taps the scan button.
+                              // (navigate to the scan screen)
+                              // navigate to ScanScreen using cupertinoPageRoute
+                              Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  transitionDuration:
+                                      const Duration(milliseconds: 200),
+                                  pageBuilder: (_, __, ___) => ScanScreen(),
+                                  transitionsBuilder:
+                                      (_, animation, __, child) {
+                                    return SlideTransition(
+                                      position: Tween<Offset>(
+                                        begin: const Offset(0, 1),
+                                        end: Offset.zero,
+                                      ).animate(CurvedAnimation(
+                                        parent: animation,
+                                        curve: Curves.easeOut,
+                                      )),
+                                      child: child,
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                            child: Container(
+                              height: 54, // Sets the height of the button.
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                                vertical: 11.0,
+                              ), // Adds padding inside the button.
+                              decoration: BoxDecoration(
+                                // Provides styling for the button.
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(
+                                        27.0)), // Makes the button round.
+                                color: Colors.white.withOpacity(0.5),
+                                boxShadow: [
+                                  // Adds a shadow effect to the button.
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    spreadRadius: 1,
+                                    blurRadius: 4,
+                                    offset: const Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: const Row(
+                                // Contains a row of widgets (icon and text).
+                                mainAxisAlignment: MainAxisAlignment
+                                    .center, // Centers the row's children on the main axis.
+                                children: [
+                                  Text(
+                                    "Enter code",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // This function will be called when the user taps the map button.
+                            // (navigate to the map screen)
+                          },
+                          child: Container(
+                            height: 54, // Sets the height of the button.
+                            width: 80, // Sets the width of the button.
+                            decoration: BoxDecoration(
+                              // Provides styling for the button.
+                              borderRadius: const BorderRadius.all(
+                                  Radius.circular(
+                                      27.0)), // Makes the button round.
+                              color: Colors.white
+                                  .withOpacity(0.5), // Sets the button's color.
+                              boxShadow: [
+                                // Adds a shadow effect to the button.
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 1,
+                                  blurRadius: 4,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: const Icon(
+                              Icons.flashlight_on, // Displays a map icon.
+                              color: Colors.white,
+                              size: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           // AppBar stacked on top
           Positioned(
             top: 0,
@@ -53,7 +175,44 @@ class _ScanScreenState extends State<ScanScreen> {
             child: AppBar(
               backgroundColor: Colors.transparent,
               elevation: 0,
-              // Add any desired AppBar content, such as title or actions
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_rounded, // Material back arrow icon
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(); // Go back when the back arrow is pressed
+                },
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: GestureDetector(
+                    onTap: () {
+                      // Handle "How it works" button press
+                    },
+                    child: const Row(
+                      children: [
+                        Text(
+                          'How it works',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        SizedBox(
+                            width: 8), // Add spacing between the text and icon
+                        Icon(
+                          Icons.info_rounded, // Info icon
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
